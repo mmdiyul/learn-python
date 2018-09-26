a9b87dba45bc4a49271869c442ded9e08cf9c1a8
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-09-26 18:51:32 +0700

    Looping python :for:

---
+++ list.py
@@ -0,0 +1,9 @@
+kota = ["Malang", "Surabaya", "Jombang", "Pasuruan", "Yogyakarta"]
+
+# print("Kota index ke-0 adalah : {} " . format(kota[0]))
+
+i = 0
+
+for list in kota:
+    i += 1
+    print("Kota ke-" + str(i) +" adalah " + list)
