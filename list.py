6b8bf54a5b6bfb049d9f9e7c6f731028701a4a97
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-09-26 19:06:00 +0700

    add list dari belakang

--- list.py
+++ list.py
@@ -1,9 +1,16 @@
 kota = ["Malang", "Surabaya", "Jombang", "Pasuruan", "Yogyakarta"]
 
-# print("Kota index ke-0 adalah : {} " . format(kota[0]))
+# Menampilkan isi ke-index
+print("Kota index ke-0 adalah : {} " . format(kota[0]))
 
-i = 0
-
+# Menampilkan seluruh isi list
+print("\nMenampilkan list")
 for list in kota:
-    i += 1
-    print("Kota ke-" + str(i) +" adalah " + list)
+    print list
+
+# Menambah data di list
+## Di belakang
+print("\nMenambah list di belakang")
+kota.append("Pekanbaru")
+for list in kota:
+    print list
